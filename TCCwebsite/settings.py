e82c4a32e4fabd2a4c633d056754c74cac47ed9d
Eric Na <hyunwoo.na@gmail.com>
2015-12-12 06:27:58 -0500

    add table and location to page, contact.php

--- TCCwebsite/settings.py
+++ TCCwebsite/settings.py
@@ -10,32 +10,43 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-"""
-HOMEPAGE_PATH = os.path.join(BASE_DIR, "homepage")
+
+PROJECT_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(PROJECT_DIR)
+WSGI_DIR = os.path.dirname(BASE_DIR)
+REPO_DIR = os.path.dirname(WSGI_DIR)
+DATA_DIR = os.environ.get('OPENSHIFT_DATA_DIR', BASE_DIR)
+HOMEPAGE_PATH = os.path.join(PROJECT_DIR, "homepage")
 TEMPLATES_PATH = os.path.join(HOMEPAGE_PATH, "templates")
 
 TEMPLATE_DIRS = (
     TEMPLATES_PATH,
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
-DEFAULT_FROM_EMAIL = 'noreply@tabchurch.us'
-"""
+import sys
+sys.path.append(os.path.join(REPO_DIR, 'libs'))
+import secrets
+SECRETS = secrets.getter(os.path.join(DATA_DIR, 'secrets.json'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&jhjsnv6ero@(s8$n1n71a6rlkjnl9_f4ja!@2d33euco1mvy1'
+#SECRET_KEY = '&jhjsnv6ero@(s8$n1n71a6rlkjnl9_f4ja!@2d33euco1mvy1'
+SECRET_KEY = SECRETS['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
-
+from socket import gethostname
+ALLOWED_HOSTS = [
+    gethostname(), # For internal OpenShift load balancer security purposes.
+    os.environ.get('OPENSHIFT_APP_DNS'), # Dynamically map to the OpenShift gear name.
+    #'example.com', # First DNS alias (set up in the app)
+    #'www.example.com', # Second DNS alias (set up in the app)
+]
 
 # Application definition
 
@@ -92,3 +103,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(HOMEPAGE_PATH, 'static')
+STATICFILE_DIRS = [os.path.join(HOMEPAGE_PATH, 'static'), ]
